cee5cdddeee3f5ae84522d09971165cfa561b61a
notbatokikic <bato.kikic@gmail.com>
2022-02-18 19:18:18 +0100

    JS added

---
+++ main.js
@@ -0,0 +1,13 @@
+const navBtnContainer = document.querySelector(".nav-btn-container");
+const searchBtn = document.querySelector(".search-btn");
+const closeBtn = document.querySelector(".close-btn");
+
+searchBtn.addEventListener("click", () => {
+  searchBox.classList.add("active");
+  navBtnContainer.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  searchBox.classList.remove("active");
+  navBtnContainer.classList.remove("active");
+});
