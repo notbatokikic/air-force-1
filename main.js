79b4691a3f4e5b3fbbc9c0f2fcc8fdbf451900b2
notbatokikic <bato.kikic@gmail.com>
2022-02-18 20:51:17 +0100

    fixed JS

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-const navBtnContainer = document.querySelector(".nav-btn-container");
-const searchBtn = document.querySelector(".search-btn");
-const closeBtn = document.querySelector(".close-btn");
+
+const navBtnContainer = document.getElementById("nav-btn-container");
+const searchBtn = document.getElementById("search-btn");
+const closeBtn = document.getElementById("close-btn");
+const searchBox = document.getElementById("search-box");
 
 searchBtn.addEventListener("click", () => {
   searchBox.classList.add("active");
@@ -11,3 +13,24 @@ closeBtn.addEventListener("click", () => {
   searchBox.classList.remove("active");
   navBtnContainer.classList.remove("active");
 });
+
+
+
+/* const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+  const form = document.getElementById('form');
+
+  if (form.style.display === 'none') {
+    // 👇️ this SHOWS the form
+    form.style.display = 'block';
+  } else {
+    // 👇️ this HIDES the form
+    form.style.display = 'none';
+  }
+}); */
+
+
+
+
+
